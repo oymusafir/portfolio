3242712ecd8f8c53e84e44e5ed7d4e93804c4891
Muhammad Adnan <oymusafir@gmail.com>
2025-09-06 18:03:43 +0500

    fix image for static

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -14,9 +14,7 @@ export default function HomePage() {
                     <div className="relative">
                         <Link href={"/"}>
                             <div className="absolute inset-0 bg-blue-400 rounded-full blur-3xl opacity-30 transition"></div>
-                            <Image
-                                height={256}
-                                width={256}
+                            <img
                                 src={'/assets/profile.webp'}
                                 alt="Muhammad Adnan"
                                 className="relative w-56 h-56 sm:w-64 sm:h-64 rounded-full border-4 border-white shadow-2xl transition-transform hover:scale-105 hover:rotate-1"
